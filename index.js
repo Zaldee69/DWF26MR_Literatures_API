c6ff6137634a3f1740dfd9c056dd9f44d15445e5
Zaldee69 <muhammadrizaldy19@gmail.com>
2021-12-04 13:58:40 +0800

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 // Get routes to the variabel
 const router = require("./src/routes");
 
